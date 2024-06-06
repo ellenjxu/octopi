46cec3717e7129f561200cf98dcbbe7d550fd0dc
heguang <heguang.lin@cephla.com>
2024-06-06 19:31:57 +0000

    add txt to specify the list of negative slides

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ import os
 
 device ='cuda' if torch.cuda.is_available() else 'cpu'
 
-@hydra.main(config_path="config/", config_name="config", version_base="1.1")
+@hydra.main(config_path="config/", config_name="config_gcloud", version_base="1.1")
 def main(cfg):
   model = hydra.utils.instantiate(cfg.model).to(device)
   model_path = os.path.join(cfg.train.out_dir, cfg.wandb.name, "model.pt")
@@ -25,7 +25,17 @@ def main(cfg):
   if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
-  for filepath in list(Path(cfg.test.data_dir).rglob('*.npy'))[:20]: # 10 neg 10 pos for testing
+  postivs = list(Path(cfg.test.data_dir_pos).rglob('*.npy'))
+  whole = list(Path(cfg.test.data_dir_whole).rglob('*.npy'))
+  
+  with open(cfg.test.data_txt_neg, 'r') as f:
+    negs_txt = f.readlines()
+  negs_txt = [x.strip().replace('.csv', '.npy') for x in negs_txt]
+  negs = [x for x in whole if any([y in x.name for y in negs_txt])]
+
+  print(f"Positives: {len(postivs)}, Negatives: {len(negs)}")
+  
+  for filepath in postivs + negs: 
     dataset_id = filepath.name.split("_cleaned")[0].split(".npy")[0]
     test_ds = SinglePatientDataset(filepath)
     test_loader = DataLoader(test_ds, batch_size=cfg.test.batch_size, shuffle=False, num_workers=cfg.test.num_workers)
