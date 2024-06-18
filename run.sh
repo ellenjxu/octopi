a7bb96f462dbcc05aca5e7c63df40e39c2ba1f9b
heguang <heguang.lin@cephla.com>
2024-06-18 21:47:18 +0000

    delete old bash script

--- run.sh
+++
@@ -1,6 +0,0 @@
-WANDB_NAME="retrain_v2"
-CONFIG="tinynn_v1"
-
-python3 train.py --config-name=$CONFIG "wandb.name=$WANDB_NAME" 
-python3 test.py  --config-name=$CONFIG "wandb.name=$WANDB_NAME"
-python3 eval.py  --config-name=$CONFIG "wandb.name=$WANDB_NAME" 
