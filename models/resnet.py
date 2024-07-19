d8aa185beee191916c6fc35d4ff9ea6da6a52909
heguang <heguang.lin@cephla.com>
2024-07-19 01:02:51 +0000

    add resnet 8

--- models/resnet.py
+++ models/resnet.py
@@ -1,18 +1,26 @@
 import torch.nn as nn
 import torchvision
+import torch
 
 class ResNet(nn.Module):
   def __init__(self, model='resnet18', n_channels=4, n_filters=64, n_classes=2, kernel_size=3, stride=1, padding=1):
     super().__init__()
     self.n_classes = n_classes
     models_dict = {
+      'resnet8': ResNet8,
       'resnet18': torchvision.models.resnet18,
       'resnet34': torchvision.models.resnet34,
       'resnet50': torchvision.models.resnet50,
       'resnet101': torchvision.models.resnet101,
       'resnet152': torchvision.models.resnet152
     }
-    self.base_model = models_dict[model](weights=None)
+
+    if model == 'resnet8':
+      self.base_model = models_dict[model](num_classes=n_classes)
+    else:
+      self.base_model = models_dict[model](weights=None)
+
+    
     self._feature_vector_dimension = self.base_model.fc.in_features
     self.base_model.conv1 = nn.Conv2d(n_channels, n_filters, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
     self.base_model = nn.Sequential(*list(self.base_model.children())[:-1]) # Remove the final fully connected layer
@@ -24,3 +32,68 @@ class ResNet(nn.Module):
     features = x.view(x.size(0), -1)
     x = self.fc(features)
     return x, features
+  
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != self.expansion * out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, self.expansion * out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * out_channels)
+            )
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = self.relu(out)
+        return out
+
+class ResNet8(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(ResNet8, self).__init__()
+        self.in_channels = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(BasicBlock, 64, 1)
+        self.layer2 = self._make_layer(BasicBlock, 128, 1, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256, 1, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(256 * BasicBlock.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
